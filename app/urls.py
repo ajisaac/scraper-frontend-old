9a049c97ef83141e042e9ab576aa21a79abe1e08
Aaron Isaac <aaron@aisaac.co>
2023-09-03 14:19:50 -0700

    Some additional filtering

--- app/urls.py
+++ app/urls.py
@@ -12,8 +12,11 @@ urlpatterns = [
     path("update_status/<int:job_id>", views.update_status, name="update_status"),
 
     # filter by status for main page    /status/new
-    path("status/<str:status>", views.status_filter, name="status_filter"),
+    path("status/<str:status>", views.main__status_filter, name="status_filter"),
+    path("main__filter_search", views.main__filter_search, name="main__filter_search"),
 
     # the main titles page              /titles
     path("titles", views.titles, name="titles"),
+    path("titles_title_search", views.titles_title_search,
+         name="titles_title_search"),
 ]
--- app/views.py
+++ app/views.py
@@ -11,23 +11,41 @@ from app.models import Job
 
 # the main page
 def index(request):
-    filter = get_job_filter(request.session)
+    filter = index__get_job_filter(request.session)
+    search_filter = {
+        'company_search': request.session['company_search'] if 'company_search' in request.session else "",
+        'title_search': request.session['title_search'] if 'title_search' in request.session else "",
+        'text_search': request.session['text_search'] if 'text_search' in request.session else "",
+    }
 
-    job = Job.objects.filter(status=filter['status']).first()
+    jobs = None
+    if (filter['status'] == "new"):
+        jobs = [Job.objects.filter(status=filter['status']).first()]
+    else:
+        jobs = Job.objects.filter(status=filter['status'])[:10]
+
     job_count = Job.objects.filter(status=filter['status']).count()
 
-    context = {"job": job, "job_count": job_count, "filter": filter}
+    context = {"jobs": jobs, "job_count": job_count,
+               "search_filter": search_filter, "filter": filter}
+
     return HttpResponse(render(request, "jobs/index.html", context))
 
 
 # when the user filters all jobs by status from main page
-def status_filter(request, status):
+def main__status_filter(request, status):
     if (not valid_status(status)):
         return HttpResponseRedirect(reverse("jobs:index"))
 
     request.session['status_filter'] = status
     return HttpResponseRedirect(reverse("jobs:index"))
 
+
+def main__filter_search(request):
+    request.session['company_search'] = request.POST["company_search"] if "company_search" in request.POST else ""
+    request.session['title_search'] = request.POST["title_search"] if "title_search" in request.POST else ""
+    request.session['text_search'] = request.POST["text_search"] if "text_search" in request.POST else ""
+    return HttpResponseRedirect(reverse("jobs:index"))
 
 #########################################################################################################
 ################################################ TITLES PAGE ############################################
@@ -36,10 +54,24 @@ def status_filter(request, status):
 
 # the titles page
 def titles(request):
-    jobs = Job.objects.order_by("title").filter(status="new")
-    jobs_size = jobs.count()
-    return HttpResponse(render(request, "jobs/titles.html", {"jobs": jobs, "jobs_size": jobs_size}))
+    title_search: str = request.session['title_search'] if 'title_search' in request.session else ""
+    print(title_search)
 
+    jobs = Job.objects.order_by("title").filter(status="new")
+    if (title_search != ""):
+        jobs = jobs.filter(title__icontains=title_search)
+
+    jobs_size = jobs.count()
+
+    context = {"jobs": jobs, "jobs_size": jobs_size,
+               "title_search": title_search}
+
+    return HttpResponse(render(request, "jobs/titles.html", context))
+
+
+def titles_title_search(request):
+    request.session['title_search'] = request.POST["title_search"]
+    return HttpResponseRedirect(reverse("jobs:titles"))
 
 #########################################################################################################
 ################################################ COMMON #################################################
@@ -65,7 +97,7 @@ def update_status(request, job_id):
 #########################################################################################################
 
 # sets up job filtering for the main job page
-def get_job_filter(session):
+def index__get_job_filter(session):
     company_search: str = session['company_search'] if 'company_search' in session else ""
     title_search: str = session['title_search'] if 'title_search' in session else ""
     text_search: str = session['text_search'] if 'text_search' in session else ""
