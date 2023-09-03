0ab196879d35c5291ba26b41ac15f61be284682b
Aaron Isaac <aaron@aisaac.co>
2023-09-03 14:19:25 -0700

    Add jinja for templating

---
+++ scraper/jinja2.py
@@ -0,0 +1,14 @@
+from jinja2 import Environment
+from django.urls import reverse
+from django.templatetags.static import static
+# for more later django installations use:
+# from django.templatetags.static import static
+
+
+def environment(**options):
+    env = Environment(**options)
+    env.globals.update({
+        "static": static,
+        "url": reverse
+    })
+    return env
--- scraper/settings.py
+++ scraper/settings.py
@@ -54,6 +54,13 @@ ROOT_URLCONF = 'scraper.urls'
 
 TEMPLATES = [
     {
+        'BACKEND': 'django.template.backends.jinja2.Jinja2',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'environment': 'scraper.jinja2.environment',
+        },
+    }, {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
         'APP_DIRS': True,
@@ -65,7 +72,7 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
             ],
         },
-    },
+    }
 ]
 
 WSGI_APPLICATION = 'scraper.wsgi.application'
